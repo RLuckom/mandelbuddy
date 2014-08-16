e77d94cac084fc0750c3b4cb42935c134bac42cc
Raphael Luckom <Raphaelluckom@gmail.com>
2014-08-16 01:21:11 -0400

    got colorization working.

--- javascript/mandelbuddy.js
+++ javascript/mandelbuddy.js
@@ -1,9 +1,25 @@
+/**
+ * partial complex number class, implementing only plus, times, and squared.
+ * Those are all I need for this
+ *
+ * @param {Number} real : real part of the complex number.
+ * @param {Number} imag : imaginary part of the complex number.
+ */
 function ComplexEnoughNumber(real, imag) {
 	this.real = real;
 	this.imag = imag;
 }
 
 ComplexEnoughNumber.prototype = {
+
+	/**
+	 * Implements addition of complex numbers. Returns a new
+	 * ComplexEnoughNumber; does not modify this one.
+	 *
+	 * @param {ComplexEnoughNumber} other : complex number to add to this
+	 *                                      one.
+	 * @return {ComplexEnoughNumber} : sum
+	 */
 	plus: function(other) {
 		return new ComplexEnoughNumber(
 			this.real + other.real,
@@ -11,17 +27,40 @@ ComplexEnoughNumber.prototype = {
 		);
 	},
 	
+	/**
+	 * Implements multiplication of complex numbers. Returns a new
+	 * ComplexEnoughNumber; does not modify this one.
+	 *
+	 * @param {ComplexEnoughNumber} other : complex number to multiply by
+	 *                                      this one.
+	 * @return {ComplexEnoughNumber} : product
+	 */
 	times: function(other) {
 		var real_part = (this.real * other.real) - (this.imag * other.imag);
 		var imag_part = (this.real * other.imag) + (this.imag * other.real);
 		return new ComplexEnoughNumber(real_part, imag_part);
 	},
 	
+	/**
+	 * Convenience method to square a number. Returns a new
+	 * ComplexEnoughNumber; does not modify this one.
+	 *
+	 * @return {ComplexEnoughNumber} : this * this
+	 */
 	squared: function() {
 		return this.times(this);
 	}
 };
 
+/**
+ * Calculates the escape time of numbers in the complex plane and makes an array
+ * of pixel data based on the provided height and width.
+ *
+ * @param {Number} height : height in pixels of desired image data
+ * @param {Number} width : width in pixels of desired image data
+ * @param {Number} iterations : number of iterations before declaring a number
+ *                              part of the Mandelbrot Set.
+ */
 function MandelBuddy(height, width, iterations) {
 	this.height = height;
 	this.width = width;
@@ -34,6 +73,12 @@ function MandelBuddy(height, width, iterations) {
 
 MandelBuddy.prototype = {
 	
+	/**
+	 * Checks whether a number is definitely not in the Mandelbrot Set.
+	 *
+	 * @param {ComplexEnoughNumber} : number to test
+	 * @return {Bool} : true if the number is definitely not in the set.
+	 */
 	bailOut: function(complexEnough) {
 		if (complexEnough.imag > 2 || complexEnough.real > 2) {
 			return true;
@@ -41,6 +86,14 @@ MandelBuddy.prototype = {
 		return false;
 	},
 	
+	/**
+	 * Returns the number of iterations before escape, or false if escape
+	 * doesn't occur.
+	 *
+	 * @param {Number} x : x-coordinate of point; real component of number
+	 * @param {Number} y : y-coordinate of point; imag component of number
+	 * @return {Number or Bool} : number of iterations to escape or false
+	 */
 	escapeTime: function(x, y) {
 		/*if (x < 0) {
 			console.log(x, y);
@@ -57,6 +110,12 @@ MandelBuddy.prototype = {
 		return false;
 	},
 	
+	/**
+	 * Makes an array of width * height length of the escape times of
+	 * numbers.
+	 *
+	 * @return {Array}
+	 */
 	makePixels: function() {
 		var pixels = [];
 		var yStep = this.imagRange / this.height;
@@ -70,7 +129,10 @@ MandelBuddy.prototype = {
 		}
 		return pixels;
 	},
-	
+
+	/**
+	 * Makes a canvas showing the Mandelbrot Set
+	 */
 	makeMandelCanvas: function() {
 		var canvas = document.createElement('canvas');
 		canvas.width = this.width;
@@ -103,7 +165,7 @@ MandelBuddy.prototype = {
 		var row = [];
 		for (var d = 0; d < pixels.length; d++){
 			row.push(pixels[d]);
-			if (d % this.width == 0) {
+			if (d % this.width === 0) {
 				console.log(row.toSource());
 				row = [];
 			}
@@ -111,16 +173,16 @@ MandelBuddy.prototype = {
 	},
 	
 	getColor: function(n) {
-		var rgb = [255, 255, 255];
-		var idx = Math.floor(this.iterations / n);
-		var hue = this.iterations % n;
+		var rgb = [0, 0, 0];
+		var idx = Math.floor((n / this.iterations) * 4);
+		var hue = Math.floor(255 - ((n / this.iterations) * 255));
 		rgb[idx] = hue;
 		return rgb;
 	}
 };
 
 function runMandelBuddy() {
-	m = new MandelBuddy(600, 900, 1200);
+	m = new MandelBuddy(600, 900, 30);
 	//m.escapeTime(-1.5, 0);
 	//m.testMandel();
 	document.body.appendChild(m.makeMandelCanvas());
